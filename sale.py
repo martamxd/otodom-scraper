56214e6838b55f34f7bb498241ddaf1aa3eaa789
martamxd <martamxd@wp.pl>
2021-04-20 10:47:40 +0200

    20.04 +zmiany

--- sale.py
+++ sale.py
@@ -20,8 +20,8 @@ def parse_page(number):
         bs2 = BeautifulSoup(page_details.content, 'html.parser')
 
 
-        for details in bs2.find_all('div', class_="css-1d9dws4 e1dlfs272"):
-            rynek = details.find_all('div',class_="css-1ytkscc ecjfvbm0")[2].get_text()
+        for details in bs2.find_all('div', class_="css-1d9dws4 egzohkh2"):
+            rynek = details.find_all('div',class_="css-1ytkscc ev4i3ak0")[2].get_text()
 
         cursor.execute('INSERT into mieszkania VALUES(?,?,?,?,?)', (name, location, price, size, rynek))
         db.commit()
@@ -35,7 +35,7 @@ if len(argv) > 1 and argv[1] == 'setup':
     cursor.execute('''CREATE TABLE mieszkania (name TEXT, location TEXT, price TEXT, size REAL, rynek TEXT) ''')
     quit()
 
-for page in range(1, 50):
+for page in range(5, 50):
     parse_page(page)
 
 db.close()
